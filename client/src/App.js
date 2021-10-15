f57b482209411d7dcfaa7079835f8b7d5a1dfbce
Toàn <64303838+bungio20k@users.noreply.github.com>
2021-10-15 16:09:09 +0700

    some change

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,130 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
-function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <Link to='/'>
+          <img src='' alt='logo'></img>
+        </Link>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/tutorial'>Tutorial</Link>
+            </li>
+
+            <li>
+              <Link to='/about'>About us</Link>
+            </li>
+
+            <li>
+              <Link to='/contact'>Contact</Link>
+            </li>
+
+            <li>
+              <Link to='/login'>Login</Link>
+            </li>
+
+            <li>
+              <Link to='/register'>Sign up</Link>
+            </li>
+          </ul>
+        </nav>
+
+
+        <Switch>
+          <Route path='/tutorial'>
+            <Tutorial />
+          </Route>
+
+          <Route path='/about'>
+            <About />
+          </Route>
+
+          <Route path='/contact'>
+            <Contact />
+          </Route>
+
+          <Route path='/login'>
+            <Login />
+          </Route>
+
+          <Route path='/register'>
+            <Register />
+          </Route>
+
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+function Home() {
+  return (
+    <div>
+      <h1>A workspace for HCMUT-er.</h1>
+      <p>More than just a notes organiser, we provide an entire workspace for those who hustle hard.</p>
+      <Link to='/register'>Get started now</Link>
+      <img srd='' alt='illustration'></img>
+    </div>
+  )
+}
+
+function Tutorial() {
+  return (
+    <div>
+      <h2>Tutorial</h2>
+    </div>
+  )
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About us</h2>
+    </div>
+  )
+}
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact us</h2>
+    </div>
+  )
+}
+
+function Login() {
+  return (
+    <div id='login'>
+      <form id='login-form' action='/vertify-login' method='post'>
+        <h2>Login here</h2>
+        <input type='text' placeholder="Username" name='user' required />
+        <input type='password' placeholder='Password' name='password' required />
+        <input type='submit' name='login' />
+      </form>
+      <Link to='/register'>Register here</Link>
+    </div>
+  )
+}
+
+function Register() {
+  return (
+    <form id='register-form' action='/register' method='post'>
+      <h2>Register here</h2>
+      <input type='text' placeholder="Username" name='user' required />
+      <input type='password' placeholder='Password' name='password' required />
+      <input type='submit' name='register' />
+    </form>
+  )
+}
--- server/index.js
+++ server/index.js
@@ -1,12 +1,22 @@
-// server/index.js
 const path = require('path');
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.post('/vertify-login', (req, res) => {
+    res.json({
+        vertify: true,
+    })
+})
+
+app.post('/register', (req, res) => {
+  res.json({
+      register: true,
+  })
+})
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
