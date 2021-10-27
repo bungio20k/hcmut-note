c519dc9d386033d49fbe8f2e47fe3259b2e54122
NhuTanCS <tan.dinh1710@hcmut.edu.vn>
2021-10-27 20:10:32 +0700

    Tan add editNode.js file, see in tutorial page

--- client/src/component/Tutorial.js
+++ client/src/component/Tutorial.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import Navbar from './Navbar'
+import React from "react";
+import Navbar from "./Navbar";
+import EditNote from "./EditNote";
 
 export default function Tutorial() {
-    return (
-        <div>
-            <Navbar />
-            <h1>Tutorial</h1>
-        </div>
-    )
+  return (
+    <div>
+      <Navbar />
+      <h1>Tutorial</h1>
+      <EditNote />
+    </div>
+  );
 }
