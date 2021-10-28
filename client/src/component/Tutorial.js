9285a56e3b232277bd647538c7c9371b59323e02
LE DUC AN <54282588+KMF-LANDER@users.noreply.github.com>
2021-10-28 08:01:02 +0700

    adding notepage to tutorial
    before log in page complete, user can access to note page. notepage will temporary added in tutorial page to bind with note popup (was add by another member in tutorial page to test funtion)

--- client/src/component/Tutorial.js
+++ client/src/component/Tutorial.js
@@ -2,12 +2,100 @@ import React from "react";
 import Navbar from "./Navbar";
 import EditNote from "./EditNote";
 
-export default function Tutorial() {
+//
+import { useState, useEffect } from 'react';
+import {nanoid} from 'nanoid';
+
+import Button from 'react-bootstrap/Button';
+
+import AddNote from './component/AddNote';
+import NotesList from './component/NotesList';
+import Search from './component/Search';
+//
+
+export default function Tutorial(handleAddNote) {
+  
+  //
+  const [notes, setNotes] = useState([
+    {
+    id: nanoid(),
+    title: "network",
+    text:"homework",
+    date: "October 26th 2021",
+    pinned: 'y',
+  },
+
+  {
+    id: nanoid(),
+    title: "dbs",
+    text:"quiz",
+    date: "October 26th 2021",
+    pinned: 'n',
+  },
+
+  {
+    id: nanoid(),
+    title: "software",
+    text:"task3",
+    date: "October 26th 2021",
+    pinned: 'y',
+  },
+
+  {
+    id: nanoid(),
+    title: "chemistry",
+    text:"midterm",
+    date: "October 26th 2021",
+    pinned: 'n',
+  },
+
+]);
+
+const addNote = (text) => {
+  const date = new Date();
+  const newNote = {
+    id: nanoid(),
+    text: text,
+    date: date.toLocaleDateString(),
+  };
+  const newNotes = [...notes, newNote];
+  setNotes(newNotes);
+};
+
+const [searchText, setSearchText] = useState('');
+  //
   return (
-    <div>
+    <div className = "container">
+    <Navbar />
+      <h1>Tutorial</h1>
+      <EditNote />
+      <Search handleSearchNote= { setSearchText}/>
+      <AddNote handleAddNote={handleAddNote}/>
+      <Button variant="primary" size="lg" active>
+    PIN
+  </Button>
+      <NotesList 
+        
+        notes = {notes.filter((note)=>(
+          (note.title.toLowerCase().includes(searchText) || note.text.toLowerCase().includes(searchText))
+          && note.pinned.includes('y')
+        ))}
+
+        handleAddNote={addNote}
+
+      />
+      <text> other note</text>
+
+      <NotesList 
+        notes = {notes.filter((note)=>(
+          (note.title.toLowerCase().includes(searchText) || note.text.toLowerCase().includes(searchText))
+          && note.pinned.includes('n')
+        ))}
+      />
+    </div>
       <Navbar />
       <h1>Tutorial</h1>
       <EditNote />
-    </div>
+    
   );
 }
