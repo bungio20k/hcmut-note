fe94e1a5a0b5d0aa21fbf83582b53fac032a058b
Quyen <daovtquyenqt707@gmail.com>
2021-10-30 16:47:58 +0700

    update UI

--- client/src/component/Login.js
+++ client/src/component/Login.js
@@ -13,53 +13,43 @@ export default function Login() {
     // };
 
     return (
+
         <div className='all'>
             <div className="Nav">
                 <Navbar />
             </div>
+            <div className='container'>
+                <div className="row">
+                    <div className='col-md-1'></div>
+                    <div className="col-md-4" id='left'>
 
-            < div className="row">
-                <div className='login'>
-                    <div>
                         <h2>Welcom back.</h2>
-                        <h5 id='logintext'> Login to access HCMUT-NOTE </h5>
+                        <h5 id="logintext"> Login to access HCMUT-NOTE </h5>
+                        <div>
+                            <input type="text" id="a" placeholder="Username" name="user" required="" />
+                        </div>
+
+                        <div>
+                            <input type="password" id="b" placeholder="Password" name="password" required="" />
+                        </div>
+
+                        <div>
+                            <input type="checkbox" id="checkbox" />Remember me</div>
+                        <input type="submit" id="subbutton" value="Login now" />
+                        <br />
+                        <hr />
+                        <br />
+                        <div>Don't have account?<a id="loginbt" href="/register">Register here</a>
+                        </div>
                     </div>
 
-                    <div>
-                        <input type='text' id='a'
-                            placeholder="Username"
-                            name='user' required />
+
+
+                    <div className="col-md-7">
+                        <img src={landpageimg} alt="Something" id='img' />
                     </div>
-
-                    <div>
-                        <input type='password' id='b'
-                            placeholder='Password'
-                            name='password' required />
-                    </div>
-
-                    <div>
-
-                        <input type='checkbox' id='checkbox' />
-                        Remember me
-                        {/* <a href='#'>Forgot password</a> */}
-                    </div>
-                    <input type='submit' value='Login now' id='subbutton' />
-                    <br />
-                    <hr />
-                    <br />
-                    <div>
-                        Don't have account?
-                        <Link to='/register' id="res">Register here</Link>
-                    </div>
-
-                </div>
-
-                <div className='rightImg'>
-                    <img src={landpageimg} alt="Something" id='ima' />
                 </div>
             </div>
-
         </div >
-
     )
 }
--- client/src/component/Register.js
+++ client/src/component/Register.js
@@ -6,60 +6,58 @@ import { Link } from 'react-router-dom';
 
 export default function Register() {
   return (
+
     <div className='all'>
       <div className="Nav">
         <Navbar />
       </div>
-
-      < div className="row">
-        <div className='login'>
-          <div>
+      <div className='container'>
+        <div className="row">
+          <div className='col-md-1'></div>
+          <div className="col-md-4" id='left'>
             <h2>Create an account.</h2>
             <h5 id='logintext'> Let's set up your account. </h5>
+            <div>
+              <input type='text' id='a'
+                placeholder="Email"
+                name='email' required />
+            </div>
+
+            <div>
+              <input type='text' id='a'
+                placeholder="Username"
+                name='user' required />
+            </div>
+
+            <div>
+              <input type='password' id='b'
+                placeholder='Password'
+                name='password' required />
+            </div>
+
+            <div>
+
+              <input type='checkbox' id='checkbox' />
+              I accept the
+              <a href="http://dontpad.com/hcmut-note-TaS"> Terms of Service and Privacy</a>
+              {/* <a href='#'>Forgot password</a> */}
+            </div>
+            <input type='submit' value='Sign up now' id='subbutton' />
+            <br />
+            <hr />
+            <br />
+            <div>
+              Already have an account?
+              <Link to='/login' id="loginbt">Login here</Link>
+            </div>
           </div>
 
-          <div>
-            <input type='text' id='a'
-              placeholder="Email"
-              name='email' required />
+
+          <div className="col-md-7">
+            <img src={landpageimg} alt="Something" id='img' />
           </div>
-
-          <div>
-            <input type='text' id='a'
-              placeholder="Username"
-              name='user' required />
-          </div>
-
-          <div>
-            <input type='password' id='b'
-              placeholder='Password'
-              name='password' required />
-          </div>
-
-          <div>
-
-            <input type='checkbox' id='checkbox' />
-            I accept the
-            <a href="http://dontpad.com/hcmut-note-TaS"> Terms of Service and Privacy</a>
-            {/* <a href='#'>Forgot password</a> */}
-          </div>
-          <input type='submit' value='Sign up now' id='subbutton' />
-          <br />
-          <hr />
-          <br />
-          <div>
-            Already have an account?
-            <Link to='/login' id="res">Login here</Link>
-          </div>
-
-        </div>
-
-        <div className='rightImg'>
-          <img src={landpageimg} alt="Something" id='ima' />
         </div>
       </div>
-
     </div >
-
   )
 }
